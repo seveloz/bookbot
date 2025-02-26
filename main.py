b36ea70e3e92efa9e4a9636e6cc56d93417d0014
seveloz <sjvlmx@gmail.com>
2025-02-26 16:11:56 -0600

    added sys

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from stats import get_num_words
 from stats import get_book_text
 from stats import count_characters
+import sys
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     character_count = count_characters(text)
     for item in character_count:
-        print(f"The '{item['char']}' character was found {item['num']} times")
-    print("--- End report ---")
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 
 main()
